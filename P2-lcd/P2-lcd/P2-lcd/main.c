a94e11f8d2faacdafcacced6f5d044bbe5cdf10a
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-15 21:25:03 -0600

    imported libraies and declared possible functions

--- P2-lcd/P2-lcd/P2-lcd/main.c
+++ P2-lcd/P2-lcd/P2-lcd/main.c
@@ -2,9 +2,20 @@
  * main.c
  * Created: 3/15/2022 8:52:37 PM
  * Author: Cuetorra
- */ 
+ * Program which turn on leds recurively
+ */
 
-#include <xc.h>
+#define F_CPU 16000000UL
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void reciveData();
+void dataProcessing();
+void characterSelection();
+void characterEnv();
 
 int main(void)
 {
