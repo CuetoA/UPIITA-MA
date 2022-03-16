6036a2d05310041f30782dd1638c2fd3a731a1ae
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-15 20:55:04 -0600

    Created P2-lcd project

---
+++ P2-lcd/P2-lcd/P2-lcd/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ * Created: 3/15/2022 8:52:37 PM
+ * Author: Cuetorra
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
