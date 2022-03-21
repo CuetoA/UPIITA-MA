56316eb3ccb2701dca913c7d7eeecf61bb600b8c
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-21 16:15:53 -0600

    Created basic P2-LCD workflow

--- P2-lcd/P2-lcd/P2-lcd/main.c
+++ P2-lcd/P2-lcd/P2-lcd/main.c
@@ -17,10 +17,29 @@ void dataProcessing();
 void characterSelection();
 void characterEnv();
 
+int val = 0;
+
 int main(void)
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	/*
+	*	- PORTC ? Input
+	*	- PORTD ? Output (LCD)
+	*	- PORTB ? Others
+	*/
+	
+	DDRC =0x00;		// Input  in C
+	PORTC=0xFF;		// 5Volts in C
+	DDRD =0xFF;		// Output in D
+	
+	while (1){
+		reciveData();
+		
+	}
+}
+
+void reciveData(){
+	val=PINC;	// Receiving value in C
+	_delay_us(1000);
+	PORTD=val;	// Delivering value in D
+	_delay_ms(1000);
 }
