f37dc408be149d93ce87c74270caead2fecbbae6
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-28 17:34:30 -0600

    Creaed project to read a keyboard

---
+++ P3-MatrixKeyboard/P3-MatrixKeyboard/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/28/2022 5:33:47 PM
+ *  Author: Cuetorra
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
