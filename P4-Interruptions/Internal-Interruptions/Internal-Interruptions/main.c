7eaf26f7831b544f75bce62646c8ff4abd255dfc
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-12 10:22:12 -0500

    started p4

---
+++ P4-Interruptions/Internal-Interruptions/Internal-Interruptions/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created: 5/12/2022 10:16:45 AM
+ *  Author: Cuetorra
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
