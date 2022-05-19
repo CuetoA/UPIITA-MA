cbbea06ef75dad9dc4e4b78697b048dc47c9f2d1
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-19 13:26:52 -0500

    finished practice

--- P6-timer-ctc-sqare-signal/P6-timer-ctc-sqare-signal/main.c
+++ P6-timer-ctc-sqare-signal/P6-timer-ctc-sqare-signal/main.c
@@ -14,32 +14,26 @@
 
 int main(void)
 {
-	cli();								// CLEAR INTERRUPTIONS
-	GTCCR = (1<<TSM) | (1<<PSRSYNC);	// STOP CLOCKS
-	
-	
 	DDRD  = 0x00;						// ENTRADAS - INT0 Y 1
 	PORTD = 0xFF;						// Pull up
 	DDRC  = 0xFF;						// LCD
 	LCD_INICIALIZA();
-	
+	_delay_ms(20);
 	
 	// CTC timer
-	OCR1A = 200;					// Ctc value
-	DDRB   = (1<<DDB1);				// Output
-	TCCR1A = (1<<COM1A0);			// Toggle when ctc
-	TCCR1B = (1<<WGM12) | (1<<CS12);// WMG12 to ctc && CS12 to 256 prescaler
-	
-	
-	// Int interruptions
-	EICRA  |= (1<<ISC01)|(1<<ISC01); // INT0 -> Flanco de subida
-	EICRA  |= (1<<ISC11)|(0<<ISC10); // INT1 -> Flanco de bajada
-	EIMSK  |= (1<<INT0) |(1<<INT1);
-	
-	
-	
+	GTCCR = (1<<TSM) | (1<<PSRSYNC);	// STOP CLOCKS
+	OCR1A  = 200;						// Ctc value
+	DDRB   = (1<<DDB1);					// Output
+	TCCR1A = (1<<COM1A0);				// Toggle when ctc
+	TCCR1B = (1<<WGM12) | (1<<CS12);	// WMG12 to ctc && CS12 to 256 prescaler	
 	GTCCR = 0;							// START CLOCLS
-	sei();								// ENABLE INTERRUPTIONS
+
+	// Int interruptions
+	cli();								// CLEAR INTERRUPTIONS
+	EICRA  |= (1<<ISC01)|(1<<ISC01); // INT0 -> Flanco de subida
+	EICRA  |= (1<<ISC11)|(1<<ISC10); // INT1 -> Flanco de bajada
+	EIMSK  |= (1<<INT0) |(1<<INT1);
+	sei();								// ENABLE INTERRUPTIONS	
 	
 	
     while(1)
@@ -50,10 +44,10 @@ int main(void)
 
 ISR(INT0_vect){
 	// Add
+	ENVIA_CADENA("add == low freq");
+	_delay_ms(1000);
 	LIMPIA_LCD();
-	ENVIA_CADENA("sumando");
-	_delay_ms(500);
-	if (OCR1A < 200){
+	if (OCR1A < 205){
 		OCR1A += 50;
 	}
 };
@@ -61,11 +55,10 @@ ISR(INT0_vect){
 
 ISR(INT1_vect){
 	// Add
+	ENVIA_CADENA("sub == up freq");
+	_delay_ms(1000);
 	LIMPIA_LCD();
-	ENVIA_CADENA("restando");
-	_delay_ms(500);
 	if (OCR1A > 50){
 		OCR1A -= 50;
 	}
-	
 };
