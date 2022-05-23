2c53399c78cc00caacbe0ffc71fce375b92b5118
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-23 13:38:55 -0500

    modification by myself finished

--- P7-pwm-correct-phase/P7-pwm-correct-phase/main.c
+++ P7-pwm-correct-phase/P7-pwm-correct-phase/main.c
@@ -24,14 +24,14 @@ int main(void)
 	GTCCR = (1<<TSM) | (1<<PSRSYNC);	// STOP CLOCKS
 	OCR1A  = 200;						// Ctc value
 	DDRB   = (1<<DDB1);					// Output
-	TCCR1A = (1<<COM1A0);				// Toggle when ctc
-	TCCR1B = (1<<WGM12) | (1<<CS12);	// WMG12 to ctc && CS12 to 256 prescaler
+	TCCR1A = (1<<COM1A0) | (1<<WGM10);				// Toggle when ctc
+	TCCR1B = (0<<WGM12)  | (0<<CS12) | (1<<CS11) | (1<<CS10);	// WMG12 to ctc && CS12 to 256 prescaler
 	GTCCR = 0;							// START CLOCLS
 
 	// Int interruptions
 	cli();								// CLEAR INTERRUPTIONS
 	EICRA  |= (1<<ISC01)|(1<<ISC01); // INT0 -> Flanco de subida
-	EICRA  |= (1<<ISC11)|(1<<ISC10); // INT1 -> Flanco de bajada
+	EICRA  |= (1<<ISC11)|(1<<ISC10); // INT1 -> Flanco de subida
 	EIMSK  |= (1<<INT0) |(1<<INT1);
 	sei();								// ENABLE INTERRUPTIONS
 	
@@ -48,8 +48,8 @@ ISR(INT0_vect){
 	_delay_ms(1000);
 	LIMPIA_LCD();
 	
-	if (OCR1A < 205){
-		OCR1A += 50;
+	if (OCR1A < 240){
+		OCR1A += 10;
 	}
 };
 
@@ -60,7 +60,7 @@ ISR(INT1_vect){
 	_delay_ms(1000);
 	LIMPIA_LCD();
 	
-	if (OCR1A > 50){
-		OCR1A -= 50;
+	if (OCR1A > 10){
+		OCR1A -= 10;
 	}
 };
